8bb7cbff815ce5ef505395c0a04ce40ffe489d33
William Tisdale <will.tisdale.31@gmail.com>
2021-04-15 12:55:31 -0500

    edited token

--- src/main/java/com/codeup/spring/Application.java
+++ src/main/java/com/codeup/spring/Application.java
@@ -15,7 +15,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
 
         //Discord Bot
-        String token = "ODMxOTQzNjYzOTg2NTQwNTg1.YHcl6g.VHz7A1JeNoy93LsOpnPs9gI9DOY";
+        String token = "your token goes here";
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
