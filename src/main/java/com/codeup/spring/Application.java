ff87cb4d8b252daf4cbb1ca5285443f207dcf6d5
William Tisdale <will.tisdale.31@gmail.com>
2021-04-15 12:44:26 -0500

    edited bot

--- src/main/java/com/codeup/spring/Application.java
+++ src/main/java/com/codeup/spring/Application.java
@@ -2,6 +2,8 @@ package com.codeup.spring;
 
 import com.codeup.spring.models.User;
 import com.codeup.spring.services.UserService;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +12,20 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        //Discord Bot
+        String token = "ODMxOTQzNjYzOTg2NTQwNTg1.YHcl6g.M3q8b7Hm8gcNmv3HNbJgegsvJ_k";
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
     }
 
 }
